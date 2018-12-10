f654ad030e5c59fd56a4521d2c23266c408ff0e1
Zhicheng Wu <zhicheng.wu@columbia.edu>
2018-12-10 16:37:32 -0500

    test checkpatch

--- user/test.c
+++ user/test.c
@@ -13,72 +13,72 @@ static const int WAIT_TIME = 5;
 
 int main(int argc, char **argv)
 {
-        pid_t pid = getpid();
-        printf("pid: %d\n", pid);
+	static const char arg1[] = "/bin/ls";
+	static const char arg2[] = "-R";
+	static const char * const argls[] = {arg1, arg2};
 
-        chdir("/ptreefs");
-        char arg1[] = "/bin/ls";
-        char arg2[] = "-R";
-        char *argls[] = {arg1, arg2};
+	chdir("/ptreefs");
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	if (pid == 0) {
+		printf("======================================\n");
+		printf("initial process tree\n");
+		printf("======================================\n");
+		execv(argls[0], argls);
+		exit(0);
+	}
 
-        pid = fork();
-        if (pid < 0) {
-                fprintf(stderr, "error: %s\n", strerror(errno));
-                return EXIT_FAILURE;
-        }
-        if (pid == 0){
-                printf("======================================\n");
-                printf("initial process tree \n");
-                printf("======================================\n");
-                execv(argls[0], argls);
-                exit(0);
-        }
-
-        while (wait(NULL) > 0);
+	while (wait(NULL) > 0)
+		;
 
 
-        for (int i = 0; i < CHILD_COUNT; ++i) {
-                pid = fork();
+	for (int i = 0; i < CHILD_COUNT; ++i) {
+		pid = fork();
 
-                if (pid < 0) {
-                        fprintf(stderr, "error: %s\n", strerror(errno));
-                        return EXIT_FAILURE;
-                }
-                if (pid == 0){
-                        sleep(WAIT_TIME);
-                        return 0;
-                }
-        }
+		if (pid < 0) {
+			fprintf(stderr, "error: %s\n", strerror(errno));
+			return EXIT_FAILURE;
+		}
+		if (pid == 0) {
+			sleep(WAIT_TIME);
+			return 0;
+		}
+	}
 
-        pid = fork();
-        if (pid < 0) {
-                fprintf(stderr, "error: %s\n", strerror(errno));
-                return EXIT_FAILURE;
-        }
-        if (pid == 0){
-                printf("======================================\n");
-                printf("After creating processes, name: test\n");
-                printf("======================================\n");
-                execv(argls[0], argls);
-                exit(0);
-        }
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	if (pid == 0) {
+		printf("======================================\n");
+		printf("After creating processes, name: test\n");
+		printf("======================================\n");
+		execv(argls[0], argls);
+		exit(0);
+	}
 
 
-        while (wait(NULL) > 0);
-        pid = fork();
-        if (pid < 0) {
-                fprintf(stderr, "error: %s\n", strerror(errno));
-                return EXIT_FAILURE;
-        }
-        if (pid == 0){
-                printf("======================================\n");
-                printf("After processes terminated\n");
-                printf("======================================\n");
-                execv(argls[0], argls);
-                exit(0);
-        }
+	while (wait(NULL) > 0)
+		;
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	if (pid == 0) {
+		printf("======================================\n");
+		printf("After processes terminated\n");
+		printf("======================================\n");
+		execv(argls[0], argls);
+		exit(0);
+	}
 
-        while (wait(NULL) > 0);
+	while (wait(NULL) > 0)
+		;
 
-        return 0;
+	return 0;
 }
